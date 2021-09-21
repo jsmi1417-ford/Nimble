ab0c77a1f928c4207441ff9796c1a8708f6c893e
John Smith <jsmi1417@ford.com>
2021-09-21 01:37:40 -0400

    use custom cwlPreconditionTesting fork

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Nimble", targets: ["Nimble"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jsmi1417-ford/CwlPreconditionTesting.git", .upToNextMajor(from: "2.0.1")),
+        .package(url: "https://github.com/jsmi1417-ford/CwlPreconditionTesting.git", .branch("master")),
     ],
     targets: [
         .target(
